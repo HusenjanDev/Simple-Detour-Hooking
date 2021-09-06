5f53ef707cce68097bceaf0146ba6209be8ec07e
Husenjan Hesenjan <Husenjan@Protonmail.com>
2021-09-06 06:10:53 +0200

    Simple Detour Hooking
    Improved the code and added comments.

--- dll/dll/DllMain.cpp
+++ dll/dll/DllMain.cpp
@@ -2,19 +2,25 @@
 #include <iostream>
 #include "detours.h"
 
+// Address of the function that will be replaced.
 DWORD Address = 0x8D24F0;
 
+// hookedFunctionOne will be attached/replaced with the original function.
 int hookedFunctionOne(int arg)
 {
 	std::cout << "Function has been hooked!" << std::endl;
 	return arg;
 }
 
+// DLL Main.
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 {
+	// The following code will execute once the DLL has been attached to an process.
 	if (dwReason == DLL_PROCESS_ATTACH) {
+		// Continuely running while loop.
 		while (true)
 		{
+			// When the f1 key has been pressed it attaches the hookedFunctionOne() onto function.
 			if (GetAsyncKeyState(VK_F1))
 			{
 				DetourTransactionBegin();
@@ -22,6 +28,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 				DetourAttach(&(LPVOID&)Address, &hookedFunctionOne);
 				DetourTransactionCommit();
 			}
+			// When the f2 key has been pressed it detatches the hookedFunctionOne() and the original function will continue running.
 			if (GetAsyncKeyState(VK_F2))
 			{
 				DetourTransactionBegin();
@@ -29,14 +36,17 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 				DetourDetach(&(LPVOID&)Address, &hookedFunctionOne);
 				DetourTransactionCommit();
 			}
+			// Completely detatches the Dynamic Link Library.
 			if (GetAsyncKeyState(VK_F3))
 			{
 				break;
 			}
 		}
 	}
+	// The following code will execute when the Dynamic Link Library is getting detatched.
 	else if (dwReason == DLL_PROCESS_DETACH)
 	{
+		// This completely frees our dynamic link library.
 		FreeLibrary(hModule);
 	}
 
--- exe/exe/Main.cpp
+++ exe/exe/Main.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <iostream>
 
+// This function will be replaced with an hooked function.
 static int functionOne(int arg)
 {
 	std::cout << "Arg: " << arg << std::endl;
@@ -9,16 +10,22 @@ static int functionOne(int arg)
 
 int main()
 {
+	// This is an function pointer and its used to get the memory address of functionOne() function.
 	int (*functionOnePtr)(int) = &functionOne;
 
-	std::cout << "Address: " << *functionOnePtr << std::endl;
-	Sleep(1500);
-
+	// We will need to continuely run the program.
 	while (true)
 	{
+		// Printing out the functionOne() memory address.
 		std::cout << "Address: " << *functionOnePtr << std::endl;
+
+		// Using the functionOne(). This will be replaced with an hookedFunction.
 		std::cout << "Sum: " << functionOne(10) << std::endl;
+
+		// These are some additional code to make our console look cleaner.
 		Sleep(400);
 		system("CLS");
 	}
+
+	return 0;
 }
